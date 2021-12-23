6473f0877cc6aa9025f0223716f5fcc86d8b8ff9
Fidan Mirizade <fidan.mirizadaa@gmail.com>
2021-12-23 11:23:30 +0400

    broken our homepage into smaller components, finish styling our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component.jsx";
+import "./homepage.styles.scss";
+
+export default function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
